10a1be91b4b5c13bc65d71a82a557646718a1282
Bruno Costa <obrnocsta@gmail.com>
2026-03-24 09:46:41 -0300

    fix: adiciona tipos nos parâmetros de algumas funções

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,51 @@
-const message: string = "Hello, TypeScript";
-console.log(message);
+const menu = [
+  { name: "Margherita", price: 8 },
+  { name: "Pepperoni", price: 10 },
+  { name: "Hawaiian", price: 10 },
+  { name: "Veggie", price: 9 },
+];
+
+let cashInRegister = 100;
+let nextId = 0;
+const orderQueue = [];
+
+const addNewPizza = (pizza) => {
+  menu.push(pizza);
+};
+
+const placeOrder = (pizzaName: string) => {
+  const pizza = menu.find((item) =>
+    item.name.toLowerCase().includes(pizzaName.toLowerCase()),
+  );
+  if (!pizza) {
+    console.error(`${pizza} does not exists in the menu`);
+    return;
+  }
+  cashInRegister += pizza.price;
+  const order = orderQueue.push({
+    id: ++nextId,
+    pizza,
+    status: "ordered",
+  });
+  return order;
+};
+
+const completeOrder = (id: number) => {
+  const index = orderQueue.findIndex((item) => item.id === id);
+  if (index === -1) {
+    return `Order not found for id: ${id}`;
+  }
+  const order = orderQueue[index];
+  order.status = "completed";
+  return order;
+};
+
+// use case
+
+addNewPizza({ name: "Banana", price: 9 });
+placeOrder("Banana");
+completeOrder(1);
+
+console.log("Menu:", menu);
+console.log("Orders:", orderQueue);
+console.log("Register: $", cashInRegister);
